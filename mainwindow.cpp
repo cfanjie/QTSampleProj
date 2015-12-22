6272b8d11f3ead65e8d96c671b287c17df3649d0
Steven Chuang <cychuangsteven@gmail.com>
2015-12-22 17:31:39 +0800

    QT Practice Init

---
+++ mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+    //@todo: hello
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    printf("button clicked");
+}
